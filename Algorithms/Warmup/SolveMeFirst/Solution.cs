6d97691036865182bf2702298dddda84bcaba66d
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-13 22:43:11 +0200

    Update Solution.cs

--- Algorithms/Warmup/SolveMeFirst/Solution.cs
+++ Algorithms/Warmup/SolveMeFirst/Solution.cs
@@ -4,8 +4,8 @@ class Solution
 {
     static void Main(String[] args)
     {
-        int val1 = Convert.ToInt32(Console.ReadLine());
-        int val2 = Convert.ToInt32(Console.ReadLine());
+        int val1 = Int32.Parse(Console.ReadLine());
+        int val2 = Int32.Parse(Console.ReadLine());
 
         int sum = SolveMeFirst(val1, val2);
 
