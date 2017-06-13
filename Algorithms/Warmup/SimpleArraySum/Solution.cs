40c5d8ff2231c621b0bfcbb0381526b72a7e66ca
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-13 23:02:59 +0200

    Update Solution.cs

--- Algorithms/Warmup/SimpleArraySum/Solution.cs
+++ Algorithms/Warmup/SimpleArraySum/Solution.cs
@@ -7,13 +7,20 @@ class Solution
         int n = Int32.Parse(Console.ReadLine());
         int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
 
+        int result = SimpleArraySum(arr);
+
+        Console.WriteLine(result);
+    }
+
+    static int SimpleArraySum(int[] arr)
+    {
         int sum = 0;
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < arr.Length; i++)
         {
             sum += arr[i];
         }
 
-        Console.WriteLine(sum);
+        return sum;
     }
 }
