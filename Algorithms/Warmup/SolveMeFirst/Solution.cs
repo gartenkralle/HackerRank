0075126dd072962bf5fc04790ee9352df4c987ef
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-13 22:19:01 +0200

    Update Solution.cs

--- Algorithms/Warmup/SolveMeFirst/Solution.cs
+++ Algorithms/Warmup/SolveMeFirst/Solution.cs
@@ -1,21 +1,19 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 
-class SolveMeFirst 
-{    
-    static void Main(String[] args) 
+class Solution
+{
+    static void Main(String[] args)
     {
         int val1 = Convert.ToInt32(Console.ReadLine());
         int val2 = Convert.ToInt32(Console.ReadLine());
-        
-        int sum = solveMeFirst(val1,val2);
-        
+
+        int sum = SolveMeFirst(val1, val2);
+
         Console.WriteLine(sum);
     }
-    
-    static int solveMeFirst(int a, int b) 
-    { 
-        return a + b;      
-    }    
-}      
+
+    static int SolveMeFirst(int a, int b)
+    {
+        return a + b;
+    }
+}
