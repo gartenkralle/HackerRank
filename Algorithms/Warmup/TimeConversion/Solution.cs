c5d39dbfa9da5a31130e300bcea79efb2aa2d5ff
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-20 21:22:08 +0200

    Create Solution.cs

---
+++ Algorithms/Warmup/TimeConversion/Solution.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time = Console.ReadLine();
+
+            string result = timeConversion(time);
+            Console.WriteLine(result);
+        }
+
+        static string timeConversion(string time)
+        {
+            int hh = Int32.Parse(time.Substring(0, 2));
+            int min = Int32.Parse(time.Substring(3, 2));
+            int sec = Int32.Parse(time.Substring(6, 2));
+
+            if ((hh != 12) && (time.Substring(time.Length - 2) == "PM"))
+                hh += 12;
+
+            if ((hh == 12) && (time.Substring(time.Length - 2) == "AM"))
+                hh -= 12;
+
+            return hh.ToString("D2") + ":" + min.ToString("D2") + ":" + sec.ToString("D2");
+        }
+    }
+}
