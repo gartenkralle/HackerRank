d79383a41c31547bfff75407d1908b086ea84194
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-24 20:29:43 +0200

    Create Solution.cs

---
+++ 30DaysOfCode/Day1DataTypes/Solution.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Day0DataTypes
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.GetCultureInfo("en-US");
+
+            int i2 = Int32.Parse(Console.ReadLine());
+            double d2 = Double.Parse(Console.ReadLine());
+            string s2 = Console.ReadLine();
+
+            Console.WriteLine(i + i2);
+            Console.WriteLine((d + d2).ToString("0.0"));
+            Console.WriteLine(s + s2);
+        }
+    }
+}
