b3b2191871a504045f00c03225ca3d23188d7b57
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-24 22:12:07 +0200

    Create Solution.cs

---
+++ 30DaysOfCode/Day8DictionariesAndMaps/Solution.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace Day8DictionariesAndMaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] nameNumber = Console.ReadLine().Split(' ');
+                string name = nameNumber[0];
+                int number = Int32.Parse(nameNumber[1]);
+
+                dict.Add(name, number);
+            }
+
+            while (true)
+            {
+                string name = Console.ReadLine();
+
+                if (dict.ContainsKey(name))
+                    Console.WriteLine(name + "=" + dict[name]);
+                else
+                    Console.WriteLine("Not found");
+            }
+        }
+    }
+}
