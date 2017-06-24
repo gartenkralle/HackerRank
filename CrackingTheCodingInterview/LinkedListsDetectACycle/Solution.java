8b8479f9c8a9f1a55e7d769bd20667e34d9a4aa5
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-24 22:04:32 +0200

    Update Solution.java

--- CrackingTheCodingInterview/LinkedListsDetectACycle/Solution.java
+++ CrackingTheCodingInterview/LinkedListsDetectACycle/Solution.java
@@ -1,20 +1,19 @@
-	boolean hasCycle(Node head) 
-	{    
-	    if (head == null)
-	        return false;
-	    
-	    Node slow = head;
-	    Node fast = head.next;
-	    
-	    while ((fast != null) && (fast.next != null))
-	    {
-	        if (slow == fast)
-	            return true;
-	        
-	        slow = slow.next;
-	        fast = fast.next.next;
-	    }
-	    
-	    return false;
-	}
-  
+boolean hasCycle(Node head) 
+{    
+    if (head == null)
+	return false;
+
+    Node slow = head;
+    Node fast = head.next;
+
+    while ((fast != null) && (fast.next != null))
+    {
+	if (slow == fast)
+	    return true;
+
+	slow = slow.next;
+	fast = fast.next.next;
+    }
+
+    return false;
+}
