c9cfa99d90b0b0333cbfbecda8881c133776ed52
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-24 22:13:20 +0200

    Create Solution.cs

---
+++ 30DaysOfCode/Day10BinaryNumbers/Solution.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Day10BinaryNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            string s = "";
+
+            while (n != 0)
+            {
+                s += (n % 2);
+                n /= 2;
+            }
+
+            int maxCount = 0;
+            int count = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '1')
+                {
+                    count++;
+
+                    if (count > maxCount)
+                        maxCount = count;
+                }
+                else
+                {
+                    count = 0;
+                }
+
+
+            }
+
+            Console.WriteLine(maxCount);
+        }
+    }
+}
