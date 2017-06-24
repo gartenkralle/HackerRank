470a8cc2554c2d02b3e299fe789ef8f501cdc652
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-24 20:33:05 +0200

    Create Solution.cs

---
+++ 30DaysOfCode/Day3IntroToConditionalStatements/Solution.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace IntroToConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+
+            if (((N % 2) == 1) || (((N % 2) == 0) && (N >= 6) && (N <= 20)))
+                Console.WriteLine("Weird");
+
+            if ((N % 2) == 0)
+            {
+                if (((N >= 2) && (N <= 5)) || (((N % 2) == 0) && (N > 20)))
+                    Console.WriteLine("Not Weird");
+            }
+        }
+    }
+}
