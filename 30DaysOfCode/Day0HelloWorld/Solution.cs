d2f66dfb6bff456d0645032ecb25e3db1df5a180
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-24 20:24:49 +0200

    Create Solution.cs

---
+++ 30DaysOfCode/Day0HelloWorld/Solution.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            String inputString;
+            inputString = Console.ReadLine();
+
+            Console.WriteLine("Hello, World.");
+            Console.WriteLine(inputString);
+
+        }
+    }
+}
