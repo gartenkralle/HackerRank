43e5429a1d2949f91e028963ef3957576b67190d
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-24 20:31:40 +0200

    Create Solution.cs

---
+++ 30DaysOfCode/Day2Operators/Solution.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Globalization;
+using System.Threading;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+            double M = Double.Parse(Console.ReadLine());
+            int T = Int32.Parse(Console.ReadLine());
+            int X = Int32.Parse(Console.ReadLine());            
+
+            double tip = M * ((double)T / 100);
+            double tax = M * ((double)X / 100);
+
+            double sum = Math.Round(M + tip + tax);
+
+            Console.WriteLine("The total meal cost is " + sum + " dollars.");
+        }
+    }
+}
