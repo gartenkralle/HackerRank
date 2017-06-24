67285fa183aecf5ceec9ee84b01672292151b6a9
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-24 20:35:29 +0200

    Create Solution.cs

---
+++ 30DaysOfCode/Day4ClassVsInstance/Solution.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Day4ClassVsInstance
+{
+    class Person
+    {
+        public int age;
+
+        public Person(int initial_Age)
+        {
+            if (initial_Age < 0)
+            {
+                age = 0;
+                Console.WriteLine("Age is not valid, setting age to 0.");
+            }
+            else
+                age = initial_Age;
+        }
+        public void amIOld()
+        {
+            if (age < 13)
+                Console.WriteLine("You are young.");
+            else if ((age >= 13) && (age < 18))
+                Console.WriteLine("You are a teenager.");
+            else
+                Console.WriteLine("You are old.");
+        }
+
+        public void yearPasses()
+        {
+            age++;
+        }
+
+        static void Main(String[] args)
+        {
+            int T = int.Parse(Console.In.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                int age = int.Parse(Console.In.ReadLine());
+                Person p = new Person(age);
+                p.amIOld();
+
+                for (int j = 0; j < 3; j++)
+                {
+                    p.yearPasses();
+                }
+
+                p.amIOld();
+                Console.WriteLine();
+            }
+        }
+    }
+}
