0b76acb052d4feb73ff8cf995735decfb33cf2df
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-24 19:47:44 +0200

    Create Solution.cs

---
+++ Algorithms/Sorting/CountingSort1/Solution.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), x => Int32.Parse(x));
+
+        int maxValue = 100;
+        int[] counts = new int[maxValue];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            counts[arr[i]]++;
+        }
+
+        Console.WriteLine(String.Join(" ", counts));
+    }
+}
