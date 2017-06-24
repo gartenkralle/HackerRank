a2d71eaaeb3323a04bc61d05447fa03d36a0f4a0
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-24 21:36:21 +0200

    Create Solution.cs

---
+++ Algorithms/Implementation/BeautifulTriplets/Solution.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace BeautifulTriplets
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nd = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+			int n = nd[0];
+			int d = nd[1];
+
+			int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+			int count = 0;
+
+			for (int i = 0; i < n; i++)
+			{
+				if ((Array.BinarySearch(a, a[i] + d) > 0) && (Array.BinarySearch(a, a[i] + 2*d) > 0))
+					count++;
+			}
+
+			Console.WriteLine(count);
+		}
+	}
+}
