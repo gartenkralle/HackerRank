4b202e20dd7fdad9b7b068930303cc07b73de053
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-24 21:47:53 +0200

    Update Solution.cs

--- Algorithms/Implementation/DesignerPDFViewer/Solution.cs
+++ Algorithms/Implementation/DesignerPDFViewer/Solution.cs
@@ -2,27 +2,27 @@ using System;
 
 namespace DesignerPDFViewer
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			byte[] h = Array.ConvertAll(Console.ReadLine().Split(' '), byte.Parse);
-			string s = Console.ReadLine();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] h = Array.ConvertAll(Console.ReadLine().Split(' '), byte.Parse);
+            string s = Console.ReadLine();
 
-			int area = 0;
-			int maxHeight = int.MinValue;
+            int area = 0;
+            int maxHeight = int.MinValue;
 
-			foreach (char c in s)
-			{
-				int currentHeight = h[Convert.ToInt32(c) - 97];
+            foreach (char c in s)
+            {
+                int currentHeight = h[Convert.ToInt32(c) - 97];
 
-				if (currentHeight > maxHeight)
-					maxHeight = currentHeight;
-			}
+                if (currentHeight > maxHeight)
+                    maxHeight = currentHeight;
+            }
 
-			area = maxHeight * s.Length;
+            area = maxHeight * s.Length;
 
-			Console.WriteLine(area);
-		}
-	}
+            Console.WriteLine(area);
+        }
+    }
 }
