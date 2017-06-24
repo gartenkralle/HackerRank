87d2cda028e191d280f6d8adb8038f62df16b97e
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-24 19:39:22 +0200

    Create Solution.cs

---
+++ Algorithms/Sorting/QuickSort1Partition/Solution.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace QickSort1Partition
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), s => Int32.Parse(s));
+
+            partition(arr);
+
+            Console.WriteLine(String.Join(" ", arr));
+        }
+
+        static void partition(int[] arr)
+        {
+            List<int> lower = new List<int>();
+            List<int> higher = new List<int>();
+            int pivot = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > pivot)
+                    higher.Add(arr[i]);
+                else
+                    lower.Add(arr[i]);
+            }
+
+            for (int i = 0; i < lower.Count; i++)
+                arr[i] = lower[i];
+
+            arr[lower.Count] = pivot;
+
+            for (int i = (lower.Count + 1); i < (lower.Count + 1 + higher.Count); i++)
+                arr[i] = higher[i-(lower.Count + 1)];
+        }
+    }
+}
