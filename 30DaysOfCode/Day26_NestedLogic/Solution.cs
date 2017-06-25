1988d2ffb7772d6841bd7270da9ccc9dd68d360e
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-25 17:27:17 +0200

    Create Solution.cs

---
+++ 30DaysOfCode/Day26_NestedLogic/Solution.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Day26_NestedLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] actualDate = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int actualD = actualDate[0];
+            int actualM = actualDate[1];
+            int actualY = actualDate[2];
+
+            int[] expectedDate = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int expectedD = expectedDate[0];
+            int expectedM = expectedDate[1];
+            int expectedY = expectedDate[2];
+
+            if (actualY > expectedY)
+            {
+                Console.WriteLine(10000);
+            }
+            else if ((actualM > expectedM) && (actualY == expectedY))
+            {
+                Console.WriteLine(500 * (actualM - expectedM));
+            }
+            else if ((actualD > expectedD) && (actualM == expectedM) && (actualY == expectedY))
+            {
+                Console.WriteLine(15 * (actualD - expectedD));
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
