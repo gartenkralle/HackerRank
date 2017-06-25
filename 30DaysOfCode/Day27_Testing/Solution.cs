279235123cf50484343a80d967a7015c73ccfd33
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-25 17:28:24 +0200

    Create Solution.cs

---
+++ 30DaysOfCode/Day27_Testing/Solution.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Day29_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5");
+
+            Console.WriteLine("5 3");
+            Console.WriteLine("-1 0 1 2 3");
+
+            Console.WriteLine("6 2");
+            Console.WriteLine("-1 0 1 2 3 4");
+
+            Console.WriteLine("7 3");
+            Console.WriteLine("-1 0 1 2 3 4 5");
+
+            Console.WriteLine("8 2");
+            Console.WriteLine("-1 0 1 2 3 4 5 6");
+
+            Console.WriteLine("9 3");
+            Console.WriteLine("-1 0 1 2 3 4 5 6 7");
+        }
+    }
+}
