8fe361d980c517984e1a29aef47e569305b00407
Clemens Schneider <clemens.schneider@posteo.de>
2017-06-28 20:34:33 +0200

    Create Solution.cs

---
+++ Security/Functions/SecurityInvolution/Solution.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace SecurityPermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            string result = "YES";
+
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[arr[i] - 1] != i + 1)
+                    result = "NO";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
