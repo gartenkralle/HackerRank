28fe5c7f9c7f245f8afb99bb77d7d5a63f3e9194
Clemens Schneider <clemens.schneider@posteo.de>
2017-07-01 17:22:42 +0200

    Create Solution.cpp

---
+++ CPP/Introduction/InputAndOutput/Solution.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() 
+{
+    int a, b, c;
+    
+    cin >> a >> b >> c;    
+    cout << a + b + c << endl;
+    
+    return 0;
+}
