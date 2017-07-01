53459dcf194bc6583057a3fe96dd2124e3a5244a
Clemens Schneider <clemens.schneider@posteo.de>
2017-07-01 17:24:44 +0200

    Create Solution.cpp

---
+++ CPP/Introduction/BasicDataTypes/Solution.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() 
+{
+	int i;
+	long l;
+	char c;
+	float f;
+	double d;
+
+	scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+
+	printf("%d\n", i);
+	printf("%ld\n", l);
+	printf("%c\n", c);
+	printf("%.3f\n", f);
+	printf("%.9lf\n", d);
+    
+    return 0;
+}
