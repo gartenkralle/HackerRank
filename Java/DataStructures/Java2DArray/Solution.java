4cf4d0cf0bbe88a0810ad81cfcbaa7c75202fb4e
Clemens Schneider <clemens.schneider@posteo.de>
2017-07-01 16:14:19 +0200

    Create Solution.java

---
+++ Java/DataStructures/Java2DArray/Solution.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Java2DArray 
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        
+        int arr[][] = new int[6][6];
+        
+        for(int i = 0; i < 6; i++)
+        {
+            for(int j = 0; j < 6; j++)
+            {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        
+        int maxSum = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                int sum = arr[i][j] + arr[i][j+1] + arr[i][j+2]	+ arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                
+                if(sum > maxSum)
+                	maxSum = sum;
+            }
+        }
+        
+        System.out.println(maxSum);
+    }
+}
