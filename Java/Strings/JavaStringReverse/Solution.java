4de93731800a4e395a6f13a506d0803ccb0f81f8
Clemens Schneider <clemens.schneider@posteo.de>
2017-07-01 15:51:37 +0200

    Create Solution.java

---
+++ Java/Strings/JavaStringReverse/Solution.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaStringReverse 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        
+        if(isPalindrom(A))
+        	System.out.println("Yes");
+        else
+        	System.out.println("No");
+    }
+
+	private static boolean isPalindrom(String a)
+	{
+		if(new StringBuilder(a).reverse().toString().equals(a))
+			return true;
+		else
+			return false;
+	}
+}
