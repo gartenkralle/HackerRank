2bbac312ef1989ba926548d8ba5e2c8964803d6e
Clemens Schneider <clemens.schneider@posteo.de>
2017-07-01 18:22:03 +0200

    Create Solution.cpp

---
+++ CPP/Introduction/Pointer/Solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void update(int *a, int *b) 
+{
+	int tempA = *a;
+
+	*a = *a + *b;
+	*b = abs(tempA - *b);
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+
+	update(&a, &b);
+
+	cout << a << endl;
+	cout << b << endl;
+
+	return 0;
+}
