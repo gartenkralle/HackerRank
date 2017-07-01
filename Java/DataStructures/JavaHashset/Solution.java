aa77428c74bc181cad57be7538271351e318d42d
Clemens Schneider <clemens.schneider@posteo.de>
2017-07-01 16:28:17 +0200

    Create Solution.java

---
+++ Java/DataStructures/JavaHashset/Solution.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class JavaHashset 
+{
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) 
+        {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+        
+        HashSet<String> set = new HashSet<>();
+        
+        for(int i = 0; i < t; i++)
+        {
+            set.add(pair_left[i] + " " + pair_right[i]);
+            System.out.println(set.size());
+        }        
+    }
+}
