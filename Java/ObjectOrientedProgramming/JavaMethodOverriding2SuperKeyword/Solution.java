537b054b51803114320164fe84d1615af1c0cb1e
Clemens Schneider <clemens.schneider@posteo.de>
2017-07-01 17:08:23 +0200

    Create Solution.java

---
+++ Java/ObjectOrientedProgramming/JavaMethodOverriding2SuperKeyword/Solution.java
@@ -0,0 +1,35 @@
+package test;
+
+class BiCycle
+{
+    String define_me()
+    {
+        return "a vehicle with pedals.";
+    }
+}
+
+class MotorCycle extends BiCycle
+{
+    @Override
+    String define_me()
+    {
+        return "a cycle with an engine.";
+    }
+    
+    MotorCycle()
+    {
+        System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+        String temp = super.define_me();
+        
+        System.out.println("My ancestor is a cycle who is "+ temp );
+    }
+    
+}
+class JavaMethodOverriding2SuperKeyword
+{
+    public static void main(String []args)
+    {        
+        MotorCycle M=new MotorCycle();
+    }
+}
