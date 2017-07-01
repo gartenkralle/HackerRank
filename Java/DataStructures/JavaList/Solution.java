6c8bc02e6f54a9e07e9790941436a48b7c0e60b6
Clemens Schneider <clemens.schneider@posteo.de>
2017-07-01 16:17:56 +0200

    Create Solution.java

---
+++ Java/DataStructures/JavaList/Solution.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaList
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int N = Integer.parseInt(in.nextLine());
+        List<Integer> L = new ArrayList<>();
+            	
+    	String[] arr = in.nextLine().split(" ");
+    	
+    	for(String s : arr)
+    		L.add(Integer.parseInt(s));
+        
+        int Q = Integer.parseInt(in.nextLine());
+        
+        for(int i = 0; i < Q; i++)
+        {
+        	String type = in.nextLine();
+        	
+        	switch(type)
+        	{
+	        	case "Insert":
+	        		arr = in.nextLine().split(" ");
+	        		int x = Integer.parseInt(arr[0]);
+	        		int y = Integer.parseInt(arr[1]);	        		
+	        		L.add(x, y);
+	        		break;
+	        		
+	        	case "Delete":
+	        		int index = Integer.parseInt(in.nextLine());
+	        		L.remove(index);
+	        		break;
+        	}
+        }
+        
+        for(int i : L)
+        	System.out.print(i + " ");
+    }
+}
