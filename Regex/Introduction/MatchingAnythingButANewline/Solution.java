6d2e4b2d4d7be66bfbfe267bf8e5c997423d905c
Clemens Schneider <clemens.schneider@posteo.de>
2017-09-30 16:58:10 +0200

    Update Solution.java

--- Regex/Introduction/MatchingAnythingButANewline/Solution.java
+++ Regex/Introduction/MatchingAnythingButANewline/Solution.java
@@ -1,7 +1,7 @@
-public class Solution {    
-
-    public static void main(String[] args) {
-        
+public class Solution 
+{
+    public static void main(String[] args)
+    {        
         Tester tester = new Tester();
         tester.check("(.){3}?\\.(.){3}?\\.(.){3}?\\.(.){3}?");     
     }
