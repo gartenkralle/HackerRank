16a711ea0700b10b02444734c408177baabc0b3f
Clemens Schneider <clemens.schneider@posteo.de>
2017-09-30 16:29:45 +0200

    Create Solution.java

---
+++ Regex/Introduction/MatchingAnythingButANewline/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Tester tester = new Tester();
+        tester.check("(.){3}?\\.(.){3}?\\.(.){3}?\\.(.){3}?");     
+    }
+}
