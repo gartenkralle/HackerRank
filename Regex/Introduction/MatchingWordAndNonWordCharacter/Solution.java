bf9f3def6b114a022f77a4b61619f572cb25ee8d
Clemens Schneider <clemens.schneider@posteo.de>
2017-10-01 19:03:53 +0200

    Create Solution.java

---
+++ Regex/Introduction/MatchingWordAndNonWordCharacter/Solution.java
@@ -0,0 +1,8 @@
+public class Solution
+{
+    public static void main(String[] args) 
+    {        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("(\\w){3}(\\W){1}(\\w){10}(\\W){1}(\\w){3}");    
+    }
+}
