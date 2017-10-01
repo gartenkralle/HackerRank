18018ce31d756b8c4ccdeda8834d3aed30a8638a
Clemens Schneider <clemens.schneider@posteo.de>
2017-10-01 12:51:35 +0200

    Create Solution.java

---
+++ Regex/Introduction/MatchingWhitespaceAndNonWhitespaceCharacter/Solution.java
@@ -0,0 +1,8 @@
+public class Solution
+{
+    public static void main(String[] args) 
+    {        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("(\\S){2}(\\s){1}(\\S){2}(\\s){1}(\\S){2}");    
+    }
+}
