7267a0ef59075262c4f124a342f44cffc5c909db
Clemens Schneider <clemens.schneider@posteo.de>
2017-10-03 11:52:34 +0200

    Create Solution.java

---
+++ Regex/Repetitions/MatchingXRepetitions/Solution.java
@@ -0,0 +1,8 @@
+public class Solution
+{
+    public static void main(String[] args) 
+    {        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[a-zA-Z02468]{40}[13579\\s]{5}$");    
+    }
+}
