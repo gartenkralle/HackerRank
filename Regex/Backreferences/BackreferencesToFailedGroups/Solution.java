f7b1cbb07bc8247108d02090c18392e31af34883
Clemens Schneider <clemens.schneider@posteo.de>
2017-10-12 15:13:35 +0200

    Create Solution.java

---
+++ Regex/Backreferences/BackreferencesToFailedGroups/Solution.java
@@ -0,0 +1,8 @@
+public class Solution
+{
+    public static void main(String[] args)
+    {        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^(\\d\\d)(-?)(\\d\\d)\\2(\\d\\d)\\2(\\d\\d)$");    
+    }
+}
