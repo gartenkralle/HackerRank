c3404b2223a769b21a273eeebaefd987039a08aa
Clemens Schneider <clemens.schneider@posteo.de>
2018-12-12 12:55:19 +0100

    Create Solution.c

---
+++ C/Functions/CalculcateTheNthTerm/Solution.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int find_nth_term(int n, int a, int b, int c)
+{
+    if (n == 1)
+        return a;
+    else if (n == 2)
+        return b;
+    else if (n == 3)
+        return c;
+    else
+        return find_nth_term(n - 1, b, c, a + b + c);
+}
+
+int main()
+{
+    int n, a, b, c;
+
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+
+    int ans = find_nth_term(n, a, b, c);
+
+    printf("%d", ans);
+
+    return 0;
+}
