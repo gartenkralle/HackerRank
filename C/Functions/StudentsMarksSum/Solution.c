d27ad167e5a531a7d72b8f28b265f6faa4f40516
Clemens Schneider <clemens.schneider@posteo.de>
2018-12-13 10:09:18 +0100

    Update Solution.c

--- C/Functions/StudentsMarksSum/Solution.c
+++ C/Functions/StudentsMarksSum/Solution.c
@@ -18,7 +18,8 @@ int marks_summation(int* marks, int number_of_students, char gender)
     return sum;
 }
 
-int main() {
+int main()
+{
     int number_of_students;
     char gender;
     int sum;
@@ -26,13 +27,15 @@ int main() {
     scanf("%d", &number_of_students);
     int *marks = (int *) malloc(number_of_students * sizeof (int));
  
-    for (int student = 0; student < number_of_students; student++) {
+    for (int student = 0; student < number_of_students; student++)
+    {
         scanf("%d", (marks + student));
     }
     
     scanf(" %c", &gender);
     sum = marks_summation(marks, number_of_students, gender);
     printf("%d", sum);
+    
     free(marks);
  
     return 0;
