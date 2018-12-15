0a1b27aebcff46ab72f843d744fd19c95e084b30
Clemens Schneider <clemens.schneider@posteo.de>
2018-12-15 17:50:00 +0100

    Create Solution.js

---
+++ Tutorials/10DaysOfJavascript/Day02_ConditionalStatementsSwitch/Solution.js
@@ -0,0 +1,46 @@
+function getLetter(s) {
+    let letter;
+
+    switch (s[0])
+    {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            letter = 'A';
+            break;
+
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'f':
+        case 'g':
+            letter = 'B';
+            break;
+
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+            letter = 'C';
+            break;
+
+        case 'n':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'v':
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+            letter = 'D';
+            break;
+    }    
+    
+    return letter;
+}
