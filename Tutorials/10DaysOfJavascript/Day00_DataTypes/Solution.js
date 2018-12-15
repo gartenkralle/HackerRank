e61e0a1b53cb828885e2b544180a8291a7232626
Clemens Schneider <clemens.schneider@posteo.de>
2018-12-15 15:06:38 +0100

    Create Solution.js

---
+++ Tutorials/10DaysOfJavascript/Day00_DataTypes/Solution.js
@@ -0,0 +1,9 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstString = 'HackerRank ';
+
+    console.log(Number(firstInteger) + Number(secondInteger));
+    console.log(Number(firstDecimal) + Number(secondDecimal));
+    console.log(firstString + secondString);
+}
