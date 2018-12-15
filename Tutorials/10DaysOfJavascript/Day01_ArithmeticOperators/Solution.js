5e0125ecd5554942349e3ed3719d109f75edfde3
Clemens Schneider <clemens.schneider@posteo.de>
2018-12-15 15:15:35 +0100

    Create Solution.js

---
+++ Tutorials/10DaysOfJavascript/Day01_ArithmeticOperators/Solution.js
@@ -0,0 +1,15 @@
+function getArea(length, width) {
+    let area;
+
+    area = length * width;
+
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter;
+
+    perimeter = (2 * length) + (2 * width);
+
+    return perimeter;
+}
