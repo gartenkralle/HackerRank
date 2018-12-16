ac2158aaded0adf8e307240894ff983a5b93327d
Clemens Schneider <clemens.schneider@posteo.de>
2018-12-16 12:19:49 +0100

    Create Solution.js

---
+++ Tutorials/10DaysOfJavascript/Day04_CreateARectangleObject/Solution.js
@@ -0,0 +1,11 @@
+function Rectangle(a, b) {
+    var rectangle =
+    {
+        length: a,
+        width: b,
+        perimeter: 2 * (a + b),
+        area: a * b
+    }
+
+    return rectangle;
+}
