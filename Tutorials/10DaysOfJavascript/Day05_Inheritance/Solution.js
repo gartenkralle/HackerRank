6d8cdd22254e6cee036c89612340442860fe67da
Clemens Schneider <clemens.schneider@posteo.de>
2018-12-16 17:38:32 +0100

    Create Solution.js

---
+++ Tutorials/10DaysOfJavascript/Day05_Inheritance/Solution.js
@@ -0,0 +1,10 @@
+Rectangle.prototype.area = function() {
+    return this.w * this.h;
+}
+
+class Square extends Rectangle {
+    constructor(size) {
+        super(size, size);
+        super.area();
+    }
+}
