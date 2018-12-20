4f88f09b8f47a7e870c1c88a5b1f77ca581ac144
Clemens Schneider <clemens.schneider@posteo.de>
2018-12-20 12:57:21 +0100

    Create Solution.cpp

---
+++ CPP/OtherConcepts/Preprocessor/Solution.cpp
@@ -0,0 +1,48 @@
+#define toStr(x) #x
+#define io(v) cin >> v
+#define FUNCTION(f, o) void f(int &m, int e){ if (e o m) m = e; }
+#define INF 10000000
+#define foreach(a,b) for(int i=0; i<n; i++)
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#if !defined toStr || !defined io || !defined FUNCTION || !defined INF
+#error Missing preprocessor definitions
+#endif 
+
+FUNCTION(minimum, < )
+FUNCTION(maximum, > )
+
+//void minimum(int m, int e)
+//{
+//    if (e < m)
+//        m = e;
+//}
+
+int main()
+{
+    int n; cin >> n;
+    vector<int> v(n);
+
+    foreach(v, i)
+    {
+        io(v)[i];
+    }
+
+    int mn = INF;
+    int mx = -INF;
+
+    foreach(v, i)
+    {
+        minimum(mn, v[i]);
+        maximum(mx, v[i]);
+    }
+
+    int ans = mx - mn;
+    cout << toStr(Result = ) << ' ' << ans;
+
+    return 0;
+}
