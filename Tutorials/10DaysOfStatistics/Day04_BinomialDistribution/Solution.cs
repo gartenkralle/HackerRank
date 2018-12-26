db5e243f2b516a37a5522bcfd912d7d9cbef1d3d
Clemens Schneider <clemens.schneider@posteo.de>
2018-12-26 21:13:34 +0100

    Create Solution.cs

---
+++ Tutorials/10DaysOfStatistics/Day04_BinomialDistribution/Solution.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace BinomialDistributionII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int k = 2;
+            double p = 0.12;
+
+            Console.WriteLine(GetProbabilityIntegral(n, k, p, 0, k).ToString("0.000"));
+            Console.WriteLine(GetProbabilityIntegral(n, k, p, k, n).ToString("0.000"));
+        }
+
+        static double GetProbabilityIntegral(int n, int k, double p, int begin, int end)
+        {
+            double probability = 0;
+
+            while (begin <= end)
+            {
+                probability += GetProbability(n, begin, p);
+                begin++;
+            }
+
+            return probability;
+        }
+
+        static double GetProbability(int n, int k, double p)
+        {
+            double q = 1 - p;
+            int nk = Faculty(n) / (Faculty(k) * Faculty(n - k));
+
+            return nk * Math.Pow(p, k) * Math.Pow(q, n - k);
+        }
+
+        static int Faculty(int n)
+        {
+            if (n == 0)
+                return 1;
+
+            return n * Faculty(n - 1);
+        }
+    }
+}
