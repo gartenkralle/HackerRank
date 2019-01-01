300f51fa40136018ed3700362be00b2f7b3fa964
Clemens Schneider <clemens.schneider@posteo.de>
2019-01-01 17:17:06 +0100

    Create Solution.rb

---
+++ Ruby/ArraysAndHashes/Addition/Solution.rb
@@ -0,0 +1,15 @@
+def end_arr_add(arr, element)
+    arr.push(element)
+end
+
+def begin_arr_add(arr, element)
+    arr.unshift(element)
+end
+
+def index_arr_add(arr, index, element)
+    arr.insert(index, element)
+end
+
+def index_arr_multiple_add(arr, index)
+    arr.insert(index, 1, 2)
+end
