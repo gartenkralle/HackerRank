021d101137b725611111f1aaed04c1e229261d3c
Clemens Schneider <clemens.schneider@posteo.de>
2019-01-03 00:00:16 +0100

    Create Solution.rb

---
+++ Ruby/ArraysAndHashes/ArraySelection/Solution.rb
@@ -0,0 +1,15 @@
+def select_arr(arr)
+    arr.select{|a| a % 2 == 1}
+end
+
+def reject_arr(arr)
+    arr.reject{|a| a % 3 == 0}
+end
+
+def delete_arr(arr)
+    arr.delete_if{|a| a < 0}
+end
+
+def keep_arr(arr)
+    arr.keep_if{|a| a >= 0}
+end
