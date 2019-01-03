d7b9f3d9ed179c84eff7658f70005b705ea32467
Clemens Schneider <clemens.schneider@posteo.de>
2019-01-03 11:31:13 +0100

    Create Solution.rb

---
+++ Ruby/ArraysAndHashes/HashInitialization/Solution.rb
@@ -0,0 +1,8 @@
+# Initialize 3 variables here as explained in the problem statement
+empty_hash = Hash.new 
+
+default_hash = Hash.new(1)
+
+hackerrank = Hash.new
+hackerrank["simmy"] = 100
+hackerrank["vivmbbs"] = 200
