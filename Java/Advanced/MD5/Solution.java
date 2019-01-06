4f4a4e336564ff2f8eb5c2add22370501f264fa5
Clemens Schneider <clemens.schneider@posteo.de>
2019-01-06 14:07:04 +0100

    Create Solution.java

---
+++ Java/Advanced/MD5/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        scanner.close();
+        
+        byte[] digest = null;
+        
+        try
+        {
+            digest = MessageDigest.getInstance("MD5").digest(s.getBytes());
+            
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        
+        for (int i = 0; i < digest.length; ++i)
+        {
+            System.out.printf("%02x", digest[i]);
+        }
+    }
+}
