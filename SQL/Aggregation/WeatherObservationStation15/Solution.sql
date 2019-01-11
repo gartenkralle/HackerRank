3e8abb205ead0944d157f7a9ecadaed91bf26849
Clemens Schneider <clemens.schneider@posteo.de>
2019-01-11 19:27:03 +0100

    Update Solution.sql

--- SQL/Aggregation/WeatherObservationStation15/Solution.sql
+++ SQL/Aggregation/WeatherObservationStation15/Solution.sql
@@ -1,1 +1,1 @@
-Select top 1 Format(LONG_W, 'N4') From Station Where LAT_N < 137.2345 Order By LAT_N Desc;
+Select Top 1 Format(LONG_W, 'N4') From Station Where LAT_N < 137.2345 Order By LAT_N Desc;
