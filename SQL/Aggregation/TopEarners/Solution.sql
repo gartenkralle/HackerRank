70ff64e81c65d0ddba015370da6ae03aa78b2ce4
Clemens Schneider <clemens.schneider@posteo.de>
2019-01-12 12:21:46 +0100

    Update Solution.sql

--- SQL/Aggregation/TopEarners/Solution.sql
+++ SQL/Aggregation/TopEarners/Solution.sql
@@ -4,7 +4,7 @@ Set @MaximumTotalEarning =
 (
   Select Top 1 Earnings 
   From (Select months*salary as [Earnings] From Employee) as Employee 
-  Group By Earnings Order By Earnings Desc
+  Order By Earnings Desc
 );
 
 Select @MaximumTotalEarning, (Select Count(employee_id) From Employee Where months*salary = @MaximumTotalEarning);
