04d3834d88986c3c9587e2d5bdf29532e83e992b
Clemens Schneider <clemens.schneider@posteo.de>
2019-01-28 11:28:35 +0100

    Create Solution.sql

---
+++ SQL/AdvancedSelect/Occupations/Solution.sql
@@ -0,0 +1,8 @@
+select Doctor, Professor, Singer, Actor
+from (select ROW_NUMBER() over (partition by Occupation order by [Name]) 'Row', [Name], Occupation from Occupations) temp
+pivot
+(
+	max([Name])
+	for Occupation
+	in (Doctor, Professor, Singer, Actor)
+) as piv
