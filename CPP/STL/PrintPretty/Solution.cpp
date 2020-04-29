96695b2f797d9b4ce15a6b4ced0673fef1e9510d
Clemens Schneider <clemens.schneider@posteo.de>
2020-04-29 18:08:25 +0200

    Create Solution.cpp

---
+++ CPP/STL/PrintPretty/Solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip> 
+
+using namespace std;
+
+int main()
+{
+	int T;
+	cin >> T;
+
+	cout << setiosflags(ios::uppercase);
+	cout << setw(0xf) << internal;
+
+	while (T--)
+	{
+		double A; cin >> A;
+		double B; cin >> B;
+		double C; cin >> C;
+
+		cout << std::setw(0)  << std::showbase << std::fixed << std::nouppercase << std::hex << static_cast<unsigned long long>(A) << endl;
+
+		std::cout.precision(2);
+		cout << std::right << std::setfill('_') << std::setw(15) << std::showpos << B << endl;
+
+		std::cout.precision(9);
+		cout << std::uppercase << std::scientific << std::noshowpos << C << endl;
+	}
+}
