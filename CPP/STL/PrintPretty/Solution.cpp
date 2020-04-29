0671508a0edcab3fd42f6869e9a7f0c07f7ca156
Clemens Schneider <clemens.schneider@posteo.de>
2020-04-29 18:10:29 +0200

    Update Solution.cpp

--- CPP/STL/PrintPretty/Solution.cpp
+++ CPP/STL/PrintPretty/Solution.cpp
@@ -5,24 +5,24 @@ using namespace std;
 
 int main()
 {
-	int T;
-	cin >> T;
+    int T;
+    cin >> T;
 
-	cout << setiosflags(ios::uppercase);
-	cout << setw(0xf) << internal;
+    cout << setiosflags(ios::uppercase);
+    cout << setw(0xf) << internal;
 
-	while (T--)
-	{
-		double A; cin >> A;
-		double B; cin >> B;
-		double C; cin >> C;
+    while (T--)
+    {
+        double A; cin >> A;
+        double B; cin >> B;
+        double C; cin >> C;
 
-		cout << std::setw(0)  << std::showbase << std::fixed << std::nouppercase << std::hex << static_cast<unsigned long long>(A) << endl;
+        cout << std::setw(0) << std::showbase << std::fixed << std::nouppercase << std::hex << static_cast<unsigned long long>(A) << endl;
 
-		std::cout.precision(2);
-		cout << std::right << std::setfill('_') << std::setw(15) << std::showpos << B << endl;
+        std::cout.precision(2);
+        cout << std::right << std::setfill('_') << std::setw(15) << std::showpos << B << endl;
 
-		std::cout.precision(9);
-		cout << std::uppercase << std::scientific << std::noshowpos << C << endl;
-	}
+        std::cout.precision(9);
+        cout << std::uppercase << std::scientific << std::noshowpos << C << endl;
+    }
 }
